e33ec17368d5b39dd763ee3ded85e5efd395aa9d
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-10-08 16:01:48 -0700

    Add files via upload

---
+++ FibonacciNumbers.java
@@ -0,0 +1,20 @@
+
+public class FibonacciNumbers {
+
+	public static void main(String[] args) {
+		int a=1;
+		int b=1;
+		int c=2;
+		System.out.print(a + " " +b + " ");
+
+		for (int i=3; i<=12; i++){
+		c=a+b;
+		b=a;
+		a=c;
+
+		System.out.print(c + " ");
+		}
+
+	}
+
+}

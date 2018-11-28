000bb779dda8c77eb2265a2b060b7da494a7008b
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-11-27 20:41:47 -0800

    ITC 136 - Fall 2018 Class Assignment #7

---
+++ Product.java
@@ -0,0 +1,68 @@
+import java.text.NumberFormat;
+
+// Design and Code a class for a generic product. 
+//The product data should include a product code, description, price and product count. 
+//In addition to the getter and setter method, the class should implement toString and 
+//also a formatted output of the price
+
+public class Product {
+
+	//defining the fields - state of each product object
+	private String code;
+	private String description;
+	private double price;
+	private static int count = 0;
+
+	//Constructors - initializing the state of the created objects
+	public Product(String code, String description, double price) {
+			this.code = code;
+			this.description = description;
+			this.price = price;	
+			count++; 
+		}
+	
+	public String getPriceFormatted() {
+        String formattedPrice = NumberFormat.getCurrencyInstance().format(this.price);
+        return formattedPrice;
+    }
+
+	//Methods
+	public String getCode() {
+		return code;
+	}
+
+	//Return the code of the product
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public static void setCount(int count) {
+		Product.count = count;
+	}
+	
+	//Returns a string representation of object
+		public String toString() {
+			return ("Product Code: " + code + "    Description: " + description + 
+					"    Price: " + getPriceFormatted());
+		}
+}
---
+++ ProductMain.java
@@ -0,0 +1,21 @@
+public class ProductMain{
+
+	public static void main(String[] args) {
+		
+		//Assigning values to product
+		Product p1 = new Product("26s4e9", "puppy dog", 250);
+		Product p2 = new Product ("93p3v2", "smart tv", 2599.95);
+		Product p3 = new Product("55b2l4", "school book", 67.45);
+		
+		//Displaying outputs
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+		//Counting the number of products
+		System.out.println("Inventory Count: " + p1.getCount());
+		
+
+	}
+
+}

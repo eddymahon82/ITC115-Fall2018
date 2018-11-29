0ed7f88555e905e73eb651d0ff90b954e4495c21
Eddy Matlock-Mahon <36487159+eddymahon82@users.noreply.github.com>
2018-11-28 17:45:19 -0800

    Updated code

--- ProductMain.java
+++ ProductMain.java
@@ -7,6 +7,8 @@ public class ProductMain{
 		Product p2 = new Product("93p3v2", "smart tv", 2599.95);
 		Product p3 = new Product("55b2l4", "school book", 67.45);
 		
+		p2.setPrice(9.99);
+		
 		//Displaying outputs
 		System.out.println(p1);
 		System.out.println(p2);
@@ -14,8 +16,5 @@ public class ProductMain{
 		
 		//Counting the number of products
 		System.out.println("Inventory Count: " + p1.getCount());
-		
-
 	}
-
 }
